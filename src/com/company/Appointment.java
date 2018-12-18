b36cb1ea7cebbeea439494b636256aa456d6c8da
Z Chen <zchen6464@bths.edu>
2018-12-18 10:30:54 -0500

    Update Appointment.java

--- src/com/company/Appointment.java
+++ src/com/company/Appointment.java
@@ -1,7 +1,11 @@
 package com.company;
 
 public class Appointment {
-    
+    private TimeInterval interval;
+    public Appointment(TimeInterval interval)
+    {
+        this.interval = interval;
+    }
     public TimeInterval getTime()
     {
         return this.interval;
